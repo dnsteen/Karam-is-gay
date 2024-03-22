5f9de9cb3f1f79e404ec2115d60e93b76867a062
David Steen <david.steen@students.jmcss.org>
2024-03-22 08:01:12 -0500

    Create datatypes.php

---
+++ datatypes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+    //String
+    $name = 'Karam loves his friend Chad.';
+
+    //Integer
+    $name = 20;
+
+    //Float
+    $name = 20.5784;
+
+    //Boolean
+
+
+    //Array
+    $names = array("Chad","Karam","David");
+    echo $names['2'];
+
+?>
+</body>
+</html>
