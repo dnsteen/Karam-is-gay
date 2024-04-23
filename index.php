efbf76fce17e702ce39646b957e3f73dcd3511ed
David Steen <david.steen@students.jmcss.org>
2024-04-23 07:52:40 -0500

    l

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    include_once 'includes/dbh.inc.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,13 +11,22 @@
     <title>Document</title>
 </head>
 <body>
-
 <?php
 
     include 'includes/header.php'
 
 ?>
+<?php
+    $sql = "SELECT * FROM users;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
 
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['user_uid'] . "<br>";
+        }
+    }
+?>
     <h1>MICHAEL SOFT</h1>
 
     <form method="GET">
